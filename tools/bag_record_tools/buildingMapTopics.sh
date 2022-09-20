0764772472cf0a44a3094d99eb08c6d3d9660636
PC <zymouse@qq.com>
2022-09-20 11:22:00 +0800

    建图话题录制脚本

--- tools/bag_record_tools/buildingMapTopics.sh
+++ tools/bag_record_tools/buildingMapTopics.sh
@@ -1,2 +1,3 @@
 # 建图需要的topic开启与录制
 #!/bin/bash
+rosbag record --split --size=2048 --buffsize=1024 $(cat ./buildingMapTopics.txt)
---
+++ tools/bag_record_tools/setup.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /home/t/Efficiency_script/tools/ros_driver/devel/setup.bash
+sleep 1
+source ~/pixautowarearchitectureproposal-sweeping_planning_functions/install/setup.bash 
