c998c8b8dffedbca49c1f56657aca64abed2190b
PC <zymouse@qq.com>
2022-09-27 11:56:13 +0800

    d

--- tools/bag_record_tools/setup.bash
+++ tools/bag_record_tools/setup.bash
@@ -3,4 +3,4 @@
 source  ~/Efficiency_script/tools/launcher/devel/setup.bash
 source  ~/Efficiency_script/tools/ros_driver/devel/setup.bash
 sleep 1
-source ~/pixautowarearchitectureproposal-sweeping_planning_functions/install/setup.bash 
+source ~/flm_workspace/pixautowarearchitectureproposal-sweeping_planning_functions/install/setup.bash 
