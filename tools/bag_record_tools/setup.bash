8aaee6099483c2761dd530102f3c76af55e20bbf
PC <zymouse@qq.com>
2022-09-27 11:54:10 +0800

    优化

--- tools/bag_record_tools/setup.bash
+++ tools/bag_record_tools/setup.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-source /home/t/Efficiency_script/tools/ros_driver/devel/setup.bash
+source  ~/Efficiency_script/tools/launcher/devel/setup.bash
+source  ~/Efficiency_script/tools/ros_driver/devel/setup.bash
 sleep 1
 source ~/pixautowarearchitectureproposal-sweeping_planning_functions/install/setup.bash 
