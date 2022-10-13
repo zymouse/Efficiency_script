75bf5ecb5a64dea1f6f5dfa4c955d02fce34c691
PC <zymouse@qq.com>
2022-10-13 14:03:28 +0800

    优化反复初始化--执行慢

--- OSMAddWgs84/OSMAddWgs84.py
+++ OSMAddWgs84/OSMAddWgs84.py
@@ -11,8 +11,26 @@ from xml.dom.minidom import parse
 from xml.etree.ElementTree import ElementTree
 from pyproj import Transformer
 
+# 返回投影坐标系WKID号码--中国区域
+def if_china_wgs84(Longitude):
+    if(72 <= Longitude <= 138):
+        '''
+        32643	WGS_1984_UTM_Zone_43N
+        32653	WGS_1984_UTM_Zone_53N
+        '''
+        return int(Longitude/6+31)+32600
+    else:
+        print("不是中国的经度")
+        return -1
+
 class OsmAddWgs84:
     def __init__(self):
+        """
+        self.collection.setAttribute("generator", 1111)      # 设置元素属性
+        self.collection.hasAttribute("generator")            # 判断元素属性是否存在
+        self.collection.getAttribute("generator")            # 获取元素属性
+        self.collection.getElementsByTagName("node")         # 按标记表示获取元素
+        """
         # yaml.warnings({'YAMLLoadWarning':False}) 
         with open(file="config.yaml",mode="r",encoding="utf-8") as f:
             config = yaml.load(f)
@@ -27,25 +45,22 @@ class OsmAddWgs84:
 
         # print ('collection属性',self.collection.nodeName,self.collection.nodeValue,self.collection.nodeType)
 
-        """
-        self.collection.setAttribute("generator", 1111)      # 设置元素属性
-        self.collection.hasAttribute("generator")            # 判断元素属性是否存在
-        self.collection.getAttribute("generator")            # 获取元素属性
-        self.collection.getElementsByTagName("node")         # 按标记表示获取元素
-        """
-       
-        
         self.utm_N=-1
-        self.origin_lat = config["origin_lat"]
-        self.origin_lon = config["origin_lon"]
-        self.origin_alt = config["origin_alt"]
-
+        self.origin_lat = float(config["origin_lat"])
+        self.origin_lon = float(config["origin_lon"])
+        self.origin_alt = float(config["origin_alt"])
+        wgs84 = "epsg:4326"
+        utm_wkid = if_china_wgs84(self.origin_lon)
+        utm_N = utm_wkid
+        self.transformer = Transformer.from_crs(wgs84, "epsg:"+str(utm_wkid))
+        self.transformer_wgs = Transformer.from_crs("epsg:"+str(utm_N), wgs84)
         self.originUtmX = 0
         self.originUtmY = 0
         self.origin2utm()
         print(self.originUtmX)
         print(self.originUtmY)
-
+        
+        
 
     
     # xml文件里，node元素的对象里，tag元素解析
@@ -82,18 +97,6 @@ class OsmAddWgs84:
         self.originUtmX,self.originUtmY= self.wgs84ToUtm(self.origin_lon, self.origin_lat)
     
 
-    # 返回投影坐标系WKID号码--中国区域
-    def if_china_wgs84(self, Longitude):
-        if(72 <= Longitude <= 138):
-            '''
-            32643	WGS_1984_UTM_Zone_43N
-            32653	WGS_1984_UTM_Zone_53N
-            '''
-            return int(Longitude/6+31)+32600
-        else:
-            print("不是中国的经度")
-            return -1
-
     # lanelet2地图点坐标(local_x,local_y)转换wgs84
     def lanelet2Pint_CoordinateToWgs84(self, point_local, utm_origin):
         # point_local lanelet2地图点坐标(local_x,local_y) 坐标
@@ -104,20 +107,17 @@ class OsmAddWgs84:
 
 
     def wgs84ToUtm(self, lon, lat):  
-        # lon lat经纬度  alt 海拔高度
-        wgs84 = "epsg:4326"
-        utm_wkid = self.if_china_wgs84(lon)
-        self.utm_N = utm_wkid
-        transformer = Transformer.from_crs(wgs84, "epsg:"+str(utm_wkid))
-        x, y = transformer.transform(lat, lon)
+        
+        # transformer = Transformer.from_crs(wgs84, "epsg:"+str(utm_wkid))
+        x, y = self.transformer.transform(lat, lon)
         # print(x)
         # print(y)
         return x, y
 
     def utmToWgs84(self, x, y):
-        wgs84 = "epsg:4326"
-        transformer = Transformer.from_crs("epsg:"+str(self.utm_N), wgs84)
-        lat, lon = transformer.transform(x, y)
+        # wgs84 = "epsg:4326"
+        # transformer = Transformer.from_crs("epsg:"+str(self.utm_N), wgs84)
+        lat, lon = self.transformer_wgs.transform(x, y)
         return lat, lon
 
 if __name__ == "__main__":
